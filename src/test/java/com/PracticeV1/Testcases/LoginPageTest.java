8abd7ed011c73ba672de631ea2ba6b1cb5e3e38f
KeerthiFlavia <c47keerufc@gmail.com>
2022-07-16 17:20:02 +0400

    my first project

---
+++ src/test/java/com/PracticeV1/Testcases/LoginPageTest.java
@@ -0,0 +1,13 @@
+package com.PracticeV1.Testcases;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+	
+	@Test
+	public void  login()
+	{
+		System.out.println("Login Successfull");
+	}
+
+}
